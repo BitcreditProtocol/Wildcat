4b9c6dbf3aedb584236b3538bb2e662ff248599b
codingpeanut157 <codingpeanut15@gmail.com>
2025-02-13 20:38:35 +0100

    add keys::Repository in surrealDB

--- src/persistence/surreal/keysets.rs
+++ src/persistence/surreal/keysets.rs
@@ -7,6 +7,7 @@ use async_trait::async_trait;
 use cdk::nuts::nut00 as cdk00;
 use cdk::nuts::nut01 as cdk01;
 use cdk::nuts::nut02 as cdk02;
+use surrealdb::RecordId;
 use surrealdb::Result as SurrealResult;
 use surrealdb::{engine::any::Any, Surreal};
 use uuid::Uuid;
@@ -16,52 +17,120 @@ use crate::credit::keys as creditkeys;
 use crate::keys;
 use crate::persistence::surreal::ConnectionConfig;
 
+// ----- keys repository
 #[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
-struct DBQuoteKeys {
-    qid: surrealdb::Uuid,
+struct DBKeys {
     info: cdk::mint::MintKeySetInfo,
     // unpacking MintKeySet because surrealdb doesn't support BTreeMap<K,V> where K is not a String
     unit: cdk00::CurrencyUnit,
     keys: HashMap<String, cdk01::MintKeyPair>,
 }
 
-fn keysentry_2_dbquoteskeys(qid: Uuid, ke: keys::KeysetEntry) -> DBQuoteKeys {
-    let (info, keyset) = ke;
-    let cdk02::MintKeySet { unit, mut keys, .. } = keyset;
-    let mut serialized_keys = HashMap::new();
-    while let Some((amount, keypair)) = keys.pop_last() {
-        // surrealDB does not accept map with keys of type anything but Strings
-        // so we need to serialize the keys to strings...
-        serialized_keys.insert(amount.to_string(), keypair);
-    }
-    DBQuoteKeys {
-        qid,
-        info,
-        unit,
-        keys: serialized_keys,
+impl From<keys::KeysetEntry> for DBKeys {
+    fn from(ke: keys::KeysetEntry) -> Self {
+        let (info, keyset) = ke;
+        let mut serialized_keys = HashMap::new();
+        let cdk02::MintKeySet { unit, mut keys, .. } = keyset;
+        while let Some((amount, keypair)) = keys.pop_last() {
+            // surrealDB does not accept map with keys of type anything but Strings
+            // so we need to serialize the keys to strings...
+            serialized_keys.insert(amount.to_string(), keypair);
+        }
+        DBKeys {
+            info,
+            unit,
+            keys: serialized_keys,
+        }
     }
 }
 
-fn dbquoteskeys_2_keysentry(dbqk: DBQuoteKeys) -> (Uuid, keys::KeysetEntry) {
-    let DBQuoteKeys {
-        qid,
-        info,
-        unit,
-        keys,
-    } = dbqk;
-    let mut keysmap: BTreeMap<cdk::Amount, cdk01::MintKeyPair> = BTreeMap::default();
-    for (val, keypair) in keys {
-        // ... and parse it back to the original type
-        let uval = val.parse::<u64>().expect("Failed to parse amount");
-        keysmap.insert(cdk::Amount::from(uval), keypair);
+impl From<DBKeys> for keys::KeysetEntry {
+    fn from(dbk: DBKeys) -> Self {
+        let DBKeys { info, unit, keys } = dbk;
+        let mut keysmap: BTreeMap<cdk::Amount, cdk01::MintKeyPair> = BTreeMap::default();
+        for (val, keypair) in keys {
+            // ... and parse them back to the original type
+            let uval = val.parse::<u64>().expect("Failed to parse amount");
+            keysmap.insert(cdk::Amount::from(uval), keypair);
+        }
+        let keyset = cdk02::MintKeySet {
+            id: info.id,
+            unit,
+            keys: cdk01::MintKeys::new(keysmap),
+        };
+        (info, keyset)
     }
-    let keyset = cdk02::MintKeySet {
-        id: info.id,
-        unit,
-        keys: cdk01::MintKeys::new(keysmap),
-    };
+}
 
-    (qid, (info, keyset))
+#[derive(Debug, Clone)]
+pub struct KeysDB {
+    pub db: Surreal<surrealdb::engine::any::Any>,
+}
+
+impl KeysDB {
+    const DB_TABLE: &'static str = "creditkeys";
+
+    pub async fn new(cfg: ConnectionConfig) -> SurrealResult<Self> {
+        let db_connection = Surreal::<Any>::init();
+        db_connection.connect(cfg.connection).await?;
+        db_connection.use_ns(cfg.namespace).await?;
+        db_connection.use_db(cfg.database).await?;
+        Ok(Self {
+            db: db_connection,
+        })
+    }
+
+    async fn store(&self, keys: keys::KeysetEntry) -> AnyResult<()> {
+        let dbkeys = DBKeys::from(keys);
+        let rid = RecordId::from_table_key(Self::DB_TABLE, dbkeys.info.id.to_string());
+        let _resp: Option<DBKeys> = self.db.insert(rid).content(dbkeys).await?;
+        Ok(())
+    }
+
+    async fn load(&self, kid: &keys::KeysetID) -> AnyResult<Option<keys::KeysetEntry>> {
+        let rid = RecordId::from_table_key(Self::DB_TABLE, kid.to_string());
+        let response: Option<DBKeys> = self.db.select(rid).await?;
+        Ok(response.map(|dbk| dbk.into()))
+    }
+}
+
+#[async_trait]
+impl keys::Repository for KeysDB {
+    async fn info(&self, kid: &keys::KeysetID) -> AnyResult<Option<cdk::mint::MintKeySetInfo>> {
+        let rid = RecordId::from_table_key(Self::DB_TABLE, kid.to_string());
+        let result: Option<cdk::mint::MintKeySetInfo> = self
+            .db
+            .query("SELECT info FROM $rid")
+            .bind(("rid", rid))
+            .await?
+            .take(0)?;
+        Ok(result)
+    }
+
+    async fn keyset(&self, kid: &keys::KeysetID) -> AnyResult<Option<cdk02::MintKeySet>> {
+        self.load(kid)
+            .await
+            .map(|res| res.map(|(_, keyset)| keyset))
+    }
+
+    async fn load(&self, kid: &keys::KeysetID) -> AnyResult<Option<keys::KeysetEntry>> {
+        self.load(kid).await
+    }
+
+    async fn store(
+        &self,
+        keyset: cdk02::MintKeySet,
+        info: cdk::mint::MintKeySetInfo,
+    ) -> AnyResult<()> {
+        self.store((info, keyset)).await
+    }
+}
+
+// ----- quote-based keys repository
+#[derive(Debug, Clone, serde::Serialize, serde::Deserialize)]
+struct DBQuoteKeys {
+    qid: surrealdb::Uuid,
+    keys: DBKeys,
 }
 
 #[derive(Debug, Clone)]
@@ -85,7 +154,7 @@ impl QuoteKeysDB {
 impl creditkeys::QuoteBasedRepository for QuoteKeysDB {
     async fn load(&self, _kid: &keys::KeysetID, qid: Uuid) -> AnyResult<Option<keys::KeysetEntry>> {
         let res: Option<DBQuoteKeys> = self.db.select((Self::DB_TABLE, qid)).await?;
-        Ok(res.map(|dbqk| dbquoteskeys_2_keysentry(dbqk).1))
+        Ok(res.map(|dbqk| dbqk.keys.into()))
     }
 
     async fn store(
@@ -94,7 +163,7 @@ impl creditkeys::QuoteBasedRepository for QuoteKeysDB {
         keyset: cdk02::MintKeySet,
         info: cdk::mint::MintKeySetInfo,
     ) -> AnyResult<()> {
-        let dbqk = keysentry_2_dbquoteskeys(qid, (info, keyset));
+        let dbqk = DBQuoteKeys{qid, keys: DBKeys::from((info, keyset))};
         let _: Option<DBQuoteKeys> = self.db.insert((Self::DB_TABLE, qid)).content(dbqk).await?;
         Ok(())
     }
