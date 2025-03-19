bac16ceaf25fc0c77d3cefda563bb4f97550ad61
codingpeanut157 <codingpeanut15@gmail.com>
2025-03-19 09:24:49 +0100

    [bcr-wdc-swap-service] rename generics for consistency

--- crates/bcr-wdc-swap-service/src/web.rs
+++ crates/bcr-wdc-swap-service/src/web.rs
@@ -7,13 +7,13 @@ use crate::error::Result;
 use crate::service::Service;
 use crate::service::{KeysService, ProofRepository};
 
-pub async fn swap_tokens<KR, PR>(
-    State(ctrl): State<Service<KR, PR>>,
+pub async fn swap_tokens<KeysSrvc, ProofRepo>(
+    State(ctrl): State<Service<KeysSrvc, ProofRepo>>,
     Json(request): Json<cdk03::SwapRequest>,
 ) -> Result<Json<cdk03::SwapResponse>>
 where
-    KR: KeysService,
-    PR: ProofRepository,
+    KeysSrvc: KeysService,
+    ProofRepo: ProofRepository,
 {
     let signatures = ctrl.swap(&request.inputs, &request.outputs).await?;
     let response = cdk03::SwapResponse { signatures };
