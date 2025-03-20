71b1316779b361c1634e25e2ce35fe111d926e3b
mjkeaton <michaeljkeaton@protonmail.com>
2025-03-20 22:35:38 +0100

    chore: handle shutdown signal

--- crates/bcr-wdc-treasury-service/src/main.rs
+++ crates/bcr-wdc-treasury-service/src/main.rs
@@ -1,3 +1,5 @@
+use tokio::signal;
+
 #[derive(Debug, serde::Deserialize)]
 struct MainConfig {
     bind_address: std::net::SocketAddr,
@@ -28,6 +30,31 @@ async fn main() {
         .expect("Failed to bind to address");
 
     axum::serve(listener, router)
+        .with_graceful_shutdown(shutdown_signal())
         .await
         .expect("Failed to start server");
+}
+
+async fn shutdown_signal() {
+  let ctrl_c = async {
+    signal::ctrl_c()
+          .await
+          .expect("failed to install Ctrl+C handler");
+  };
+
+  #[cfg(unix)]
+  let terminate = async {
+      signal::unix::signal(signal::unix::SignalKind::terminate())
+          .expect("failed to install signal handler")
+          .recv()
+          .await;
+  };
+
+  #[cfg(not(unix))]
+  let terminate = std::future::pending::<()>();
+
+  tokio::select! {
+      _ = ctrl_c => {},
+      _ = terminate => {},
+  }
 }
