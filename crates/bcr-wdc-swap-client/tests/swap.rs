b82796ab8306f4ab9a16a7df6998b7ac7119e8f2
codingpeanut157 <codingpeanut15@gmail.com>
2025-03-24 15:43:08 +0100

    [bcr-wdc-swap-client] add integration tests

---
+++ crates/bcr-wdc-swap-client/tests/swap.rs
@@ -0,0 +1,30 @@
+// ----- standard library imports
+// ----- extra library imports
+use bcr_wdc_keys::test_utils as key_utils;
+use bcr_wdc_swap_client::SwapClient;
+use cashu::Amount;
+// ----- local imports
+
+#[tokio::test]
+async fn swap() {
+    let (server, keys_service) = bcr_wdc_swap_service::test_utils::build_test_server();
+    let server_url = server.server_address().expect("address");
+    let client = SwapClient::new(server_url).expect("client");
+
+    let keys_entry = key_utils::generate_keyset();
+    keys_service
+        .keys
+        .keys
+        .store(keys_entry.clone())
+        .await
+        .expect("store");
+
+    let amounts = vec![Amount::from(8_u64)];
+    let blinds = bcr_wdc_swap_service::utils::generate_blinds(&keys_entry.1, &amounts)
+        .into_iter()
+        .map(|bbb| bbb.0)
+        .collect();
+    let proofs = bcr_wdc_swap_service::utils::generate_proofs(&keys_entry.1, &amounts);
+
+    client.swap(proofs, blinds).await.expect("swap");
+}
