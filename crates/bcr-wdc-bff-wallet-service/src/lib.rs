109ee420c2c2aa9a9b7375f11aa59c673b90ba8a
mjkeaton <michaeljkeaton@protonmail.com>
2025-03-24 12:31:10 +0100

    chore(bff-wallet): add mint url

--- crates/bcr-wdc-bff-wallet-service/src/lib.rs
+++ crates/bcr-wdc-bff-wallet-service/src/lib.rs
@@ -1,8 +1,12 @@
+use std::str::FromStr;
 // ----- standard library imports
 // ----- extra library imports
 use axum::Router;
 use axum::extract::FromRef;
-use axum::routing::{get};
+use axum::routing::get;
+use cashu::mint_url::MintUrl;
+use cdk::HttpClient;
+use cdk::wallet::client::MintConnector;
 
 mod keys;
 
@@ -11,6 +15,7 @@ type ProdKeysService = crate::keys::RESTClient;
 #[derive(Clone, Debug, serde::Deserialize)]
 pub struct AppConfig {
     keys_client: crate::keys::KeysClientConfig,
+    cdk_mint_url: String,
 }
 
 #[derive(Clone, FromRef)]
@@ -18,11 +23,29 @@ pub struct AppController {}
 
 impl AppController {
     pub async fn new(cfg: AppConfig) -> Self {
-        let AppConfig { keys_client } = cfg;
+        let AppConfig {
+            keys_client,
+            cdk_mint_url,
+        } = cfg;
 
         let _keys_repo = ProdKeysService::new(keys_client)
             .await
             .expect("Failed to create keys client");
+
+        let _mint_url =
+            MintUrl::from_str(cdk_mint_url.as_str()).expect("Failed to create mint url");
+
+        let _mint_client = HttpClient::new(_mint_url);
+
+        let info = _mint_client.get_mint_info().await;
+        log::info!(
+            "Connected to mint: {}",
+            info.map(|it| it.version)
+                .unwrap()
+                .or(None)
+                .unwrap()
+                .to_string()
+        );
 
         Self {}
     }
