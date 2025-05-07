c2cd0988b13d4cb709906ee01cf109f3404319ac
codingpeanut157 <codingpeanut15@gmail.com>
2025-05-07 18:18:51 +0200

    [bcr-wdc-wallet-aggregator] change shutdown_signal according to codebase pattern

--- crates/bcr-wdc-wallet-aggregator/src/main.rs
+++ crates/bcr-wdc-wallet-aggregator/src/main.rs
@@ -42,10 +42,25 @@ async fn main() {
 }
 
 async fn shutdown_signal() {
-    let mut terminate = signal(SignalKind::terminate()).expect("failed to install signal handler");
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
     tokio::select! {
-        _ = ctrl_c() => {},
-        _ = terminate.recv() => {},
+        _ = ctrl_c => {},
+        _ = terminate => {},
     }
-    log::info!("Shutting down...");
 }
