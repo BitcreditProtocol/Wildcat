5371ac8b8c461e22d50547b80c2dda7da87bdba5
stefanbitcr <stefanbitcr@gmail.com>
2025-05-08 22:17:09 +0200

    [bcr-wdc-e2e-tests] Switch to binary for e2e tests

--- crates/bcr-wdc-e2e-tests/src/main.rs
+++ crates/bcr-wdc-e2e-tests/src/main.rs
@@ -1,41 +1,49 @@
 // ----- standard library imports
-use chrono::NaiveTime;
+
 // ----- extra library imports
-use tracing::info;
-use tracing_subscriber::{filter::LevelFilter, prelude::*};
-use bcr_wdc_webapi::quotes::{
-    IdentityPublicData, ContactType,BillInfo, EnquireRequest,
-    UpdateQuoteRequest, UpdateQuoteResponse, StatusReply, InfoReply
-};
 use bcr_wdc_webapi::keys::ActivateKeysetRequest;
+use bcr_wdc_webapi::quotes::{
+    BillInfo, EnquireRequest, IdentityPublicData, StatusReply, UpdateQuoteRequest,
+    UpdateQuoteResponse,
+};
+use bdk_wallet::serde_json;
 use bitcoin::secp256k1::Keypair;
+use cashu::nuts::nut02 as cdk02;
 use cashu::{Amount, MintBolt11Request, MintBolt11Response};
 use rand::Rng;
-use bdk_wallet::serde_json;
-use cashu::nuts::nut02 as cdk02;
+use tracing::info;
+use tracing_subscriber::filter::LevelFilter;
 // ----- local modules
 
 // ----- end imports
 
+#[derive(Debug, serde::Deserialize)]
+struct MainConfig {
+    wallet_service: String,
+    admin_service: String,
+    key_service: String,
+    quote_service: String,
+}
+
 fn setup_tracing() {
-    tracing_subscriber::fmt().with_max_level(LevelFilter::INFO).init();
-    
+    tracing_subscriber::fmt()
+        .with_max_level(LevelFilter::OFF)
+        .init();
 }
 
 fn random_ebill() -> (Keypair, BillInfo, bitcoin::secp256k1::schnorr::Signature) {
-
     let bill_id = bcr_wdc_webapi::test_utils::random_bill_id();
-    let (_,drawee) = bcr_wdc_webapi::test_utils::random_identity_public_data();
-    let (_,drawer) = bcr_wdc_webapi::test_utils::random_identity_public_data();
-    let (_,payee) = bcr_wdc_webapi::test_utils::random_identity_public_data();
+    let (_, drawee) = bcr_wdc_webapi::test_utils::random_identity_public_data();
+    let (_, drawer) = bcr_wdc_webapi::test_utils::random_identity_public_data();
+    let (_, payee) = bcr_wdc_webapi::test_utils::random_identity_public_data();
 
     let endorsees_size = rand::thread_rng().gen_range(0..3);
     let mut endorsees: Vec<IdentityPublicData> = Vec::with_capacity(endorsees_size);
 
-    let (endorser_kp,endorser) = bcr_wdc_webapi::test_utils::random_identity_public_data();
+    let (endorser_kp, endorser) = bcr_wdc_webapi::test_utils::random_identity_public_data();
     endorsees.push(endorser);
 
-    let owner_key =  bcr_wdc_utils::keys::test_utils::generate_random_keypair();
+    let owner_key = bcr_wdc_utils::keys::test_utils::generate_random_keypair();
 
     let amount = Amount::from(rand::thread_rng().gen_range(1000..100000));
 
@@ -52,17 +60,17 @@ fn random_ebill() -> (Keypair, BillInfo, bitcoin::secp256k1::schnorr::Signature)
     let signature = bcr_wdc_utils::keys::schnorr_sign_borsh_msg_with_key(&bill, &endorser_kp)
         .expect("schnorr_sign_borsh_msg_with_key");
 
-    (owner_key,bill, signature)
+    (owner_key, bill, signature)
 }
-
 
 fn random_date() -> String {
     let start = chrono::Utc::now() + chrono::Duration::days(365);
     let days = rand::thread_rng().gen_range(0..365);
-    (start + chrono::Duration::days(days) ).to_rfc3339()
+    (start + chrono::Duration::days(days)).to_rfc3339()
 }
 
-fn get_amounts(mut targ: u64) -> Vec<u64> { // TODO see if there is an existing cashu implementation
+fn get_amounts(mut targ: u64) -> Vec<u64> {
+    // TODO see if there is an existing cashu implementation
     let mut coins = Vec::new();
     let mut bit_position = 0;
     while targ > 0 {
@@ -76,10 +84,14 @@ fn get_amounts(mut targ: u64) -> Vec<u64> { // TODO see if there is an existing 
 }
 
 pub fn generate_blinds(
-    keyset_id : cdk02::Id,
+    keyset_id: cdk02::Id,
     amounts: &[Amount],
-) -> Vec<(cashu::BlindedMessage, cashu::secret::Secret, cashu::SecretKey)> {
-    let mut blinds= Vec::new();
+) -> Vec<(
+    cashu::BlindedMessage,
+    cashu::secret::Secret,
+    cashu::SecretKey,
+)> {
+    let mut blinds = Vec::new();
     for amount in amounts {
         let blind = bcr_wdc_utils::keys::test_utils::generate_blind(keyset_id, *amount);
         blinds.push(blind);
@@ -87,13 +99,15 @@ pub fn generate_blinds(
     blinds
 }
 
-
-#[tokio::test]
-async fn can_mint_ebill(){
-
+async fn can_mint_ebill(cfg: &MainConfig) {
     setup_tracing();
 
-    let (owner_key,bill, signature) = random_ebill();
+    let wallet_service_url = cfg.wallet_service.clone();
+    let admin_service_url = cfg.admin_service.clone();
+    let key_service_url = cfg.key_service.clone();
+    let quote_service_url = cfg.quote_service.clone();
+
+    let (owner_key, bill, signature) = random_ebill();
 
     let request = EnquireRequest {
         content: bill,
@@ -104,18 +118,28 @@ async fn can_mint_ebill(){
     let request_json = serde_json::to_string(&request).expect("Failed to serialize request");
     let bill_amount = request.content.sum;
 
-    info!(bill_amount = bill_amount, bill_id = request.content.id, "Bill created");
+    info!(
+        bill_amount = bill_amount,
+        bill_id = request.content.id,
+        "Bill created"
+    );
 
     let client = reqwest::Client::new();
 
     info!("Requesting to mint the bill");
-    let response = client.post("http://localhost:4343/v1/mint/credit/quote")
+    let mint_credit_quote_url = format!("{}/v1/mint/credit/quote", wallet_service_url);
+    let response = client
+        .post(mint_credit_quote_url)
         .header("Content-Type", "application/json")
         .body(request_json)
         .send()
-        .await.unwrap();
+        .await
+        .unwrap();
 
-    let enquire_reply = response.json::<bcr_wdc_webapi::quotes::EnquireReply>().await.unwrap();
+    let enquire_reply = response
+        .json::<bcr_wdc_webapi::quotes::EnquireReply>()
+        .await
+        .unwrap();
     let quote_id = enquire_reply.id;
 
     info!(quote_id = ?quote_id, "Mint Request Accepted, waiting for admin to process");
@@ -123,16 +147,21 @@ async fn can_mint_ebill(){
     let one_year_from_now = chrono::Utc::now() + chrono::Duration::days(365);
     let discounted_offer = bill_amount * 99 / 100;
     let update_quote_request_payload = UpdateQuoteRequest::Offer {
-        discounted: bitcoin::Amount::from_sat(discounted_offer ), 
-        ttl: Some( one_year_from_now ),
+        discounted: bitcoin::Amount::from_sat(discounted_offer),
+        ttl: Some(one_year_from_now),
     };
 
     let update_quote_request_json = serde_json::to_string(&update_quote_request_payload)
         .expect("Failed to serialize UpdateQuoteRequest");
 
-    let admin_update_quote_url = format!("http://localhost:4242/v1/admin/credit/quote/{}", quote_id);
-    info!(discounted=discounted_offer, "Admin sending discounted offer");
-    let update_response = client.post(&admin_update_quote_url)
+    let admin_update_quote_url =
+        format!("{}/v1/admin/credit/quote/{}", admin_service_url, quote_id);
+    info!(
+        discounted = discounted_offer,
+        "Admin sending discounted offer"
+    );
+    let update_response = client
+        .post(&admin_update_quote_url)
         .header("Content-Type", "application/json")
         .body(update_quote_request_json)
         .send()
@@ -149,12 +178,9 @@ async fn can_mint_ebill(){
         }
     }
 
-    let mint_quote_status_url = format!("http://localhost:4002/v1/mint/credit/quote/{}", quote_id);
+    let mint_quote_status_url = format!("{}/v1/mint/credit/quote/{}", quote_service_url, quote_id);
     info!("Getting mint quote status from: {}", mint_quote_status_url);
-    let mint_status_response = client.get(&mint_quote_status_url)
-        .send()
-        .await
-        .unwrap();
+    let mint_status_response = client.get(&mint_quote_status_url).send().await.unwrap();
 
     let mint_quote_status_reply = mint_status_response.json::<StatusReply>().await.unwrap();
 
@@ -168,16 +194,17 @@ async fn can_mint_ebill(){
         StatusReply::Offered { keyset_id, .. } => Some(keyset_id),
         StatusReply::Accepted { keyset_id, .. } => Some(keyset_id),
         _ => None,
-    }.unwrap();
+    }
+    .unwrap();
 
-    
     let activate_request_payload = ActivateKeysetRequest { qid: quote_id };
     let activate_request_json = serde_json::to_string(&activate_request_payload)
         .expect("Failed to serialize ActivateKeysetRequest");
 
     info!("Activating keyset for quote_id: {}", quote_id);
+    let activate_url = format!("{}/v1/admin/keys/activate", admin_service_url);
     let activate_response = client
-        .post("http://localhost:4242/v1/admin/keys/activate")
+        .post(activate_url)
         .header("Content-Type", "application/json")
         .body(activate_request_json)
         .send()
@@ -189,56 +216,72 @@ async fn can_mint_ebill(){
         activate_response.status()
     );
 
-    // let keyset_info_url = format!("http://localhost:4001/v1/keysets/{}", keyset_id);
-    // info!("Getting keyset info from: {}", keyset_info_url);
-    // let keyset_info_response = client
-    //     .get(&keyset_info_url)
-    //     .send()
-    //     .await
-    //     .unwrap();
-
-    // let keyset_info = keyset_info_response
-    //     .json::<cdk02::KeySetInfo>()
-    //     .await
-    //     .unwrap();
-    
-    // assert!(keyset_info.active);
-
-    // Json<cdk02::KeysetResponse>
-    //     url = f"http://localhost:4343/v1/keysets"
-    // response = requests.get(url)
-    // print(response.text)
-    // get all keysets from mint and see if our keyset is in the list and active
-    let keysets_response = client.get("http://localhost:4343/v1/keysets").send().await.unwrap();
-    let keysets = keysets_response.json::<cdk02::KeysetResponse>().await.unwrap();
+    let keysets_url = format!("{}/v1/keysets", wallet_service_url);
+    let keysets_response = client.get(keysets_url).send().await.unwrap();
+    let keysets = keysets_response
+        .json::<cdk02::KeysetResponse>()
+        .await
+        .unwrap();
     assert!(keysets.keysets.iter().any(|ks| ks.id == keyset_id));
     assert!(keysets.keysets.iter().any(|ks| ks.active));
-    let keyset_info = keysets.keysets.iter().find(|ks| ks.id == keyset_id).unwrap();
+    let keyset_info = keysets
+        .keysets
+        .iter()
+        .find(|ks| ks.id == keyset_id)
+        .unwrap();
     assert!(keyset_info.active);
 
     info!(keyset_info_id = ?keyset_info.id, "Confirmed active keyset");
 
-    let amounts = get_amounts(bill_amount).iter().map(|a| cashu::Amount::from(*a)).collect::<Vec<_>>();
-    let blinds =  generate_blinds(keyset_info.id, &amounts);
+    let amounts = get_amounts(bill_amount)
+        .iter()
+        .map(|a| cashu::Amount::from(*a))
+        .collect::<Vec<_>>();
+    let blinds = generate_blinds(keyset_info.id, &amounts);
     let blinded_messages = blinds.iter().map(|b| b.0.clone()).collect::<Vec<_>>();
 
     info!("Signing NUT20 mint request");
-    let mut req = MintBolt11Request { quote : quote_id, outputs : blinded_messages, signature : None};
+    let mut req = MintBolt11Request {
+        quote: quote_id,
+        outputs: blinded_messages,
+        signature: None,
+    };
     req.sign(owner_key.secret_key().into()).unwrap();
 
     info!("Sending NUT20 mint request");
-    let mint_response = client.post("http://localhost:4001/v1/mint/ebill")
+    let mint_url = format!("{}/v1/mint/ebill", key_service_url);
+    let mint_response = client
+        .post(mint_url)
         .header("Content-Type", "application/json")
         .body(serde_json::to_string(&req).unwrap())
         .send()
-        .await.unwrap();
+        .await
+        .unwrap();
 
     let mint_response_body = mint_response.json::<MintBolt11Response>().await.unwrap();
     info!("Mint response: {:?}", mint_response_body);
 
     let blinded_signatures = mint_response_body.signatures;
-    
-    let total_amount = blinded_signatures.iter().map(|s| u64::from(s.amount) ).sum::<u64>();
-    assert_eq!(total_amount, bill_amount);
 
+    let total_amount = blinded_signatures
+        .iter()
+        .map(|s| u64::from(s.amount))
+        .sum::<u64>();
+    assert_eq!(total_amount, bill_amount);
+}
+
+fn main() {
+    let settings = config::Config::builder()
+        .add_source(config::File::with_name("config.toml"))
+        .add_source(config::Environment::with_prefix("WALLET_AGGREGATOR"))
+        .build()
+        .expect("Failed to build wildcat config");
+
+    let cfg: MainConfig = settings
+        .try_deserialize()
+        .expect("Failed to parse wildcat config");
+
+    tokio::runtime::Runtime::new()
+        .unwrap()
+        .block_on(can_mint_ebill(&cfg));
 }
