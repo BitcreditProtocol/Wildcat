69d92cb5ad49638675da59315c23ad7f63c2f2cd
stefanbitcr <stefanbitcr@gmail.com>
2025-05-09 07:51:19 +0200

    [bcr-wdc-e2e-tests] Clean up code

---
+++ crates/bcr-wdc-e2e-tests/src/endpoints.rs
@@ -0,0 +1,54 @@
+// ----- standard library imports
+use std::marker::PhantomData;
+// ----- extra library imports
+use reqwest::Url;
+// ----- local modules
+// ----- end imports
+
+pub struct Endpoints<T> {
+    base_url: String,
+    _marker: PhantomData<T>,
+}
+
+impl<T> Endpoints<T> {
+    pub fn new(base_url: String) -> Self {
+        Self {
+            base_url,
+            _marker: PhantomData,
+        }
+    }
+    fn url(&self, path: &str) -> Url {
+        Url::parse(&format!("{}/{}", self.base_url, path)).unwrap()
+    }
+}
+
+pub struct WalletService {}
+pub struct AdminService {}
+pub struct KeyService {}
+
+impl Endpoints<WalletService> {
+    pub fn mint_credit_quote_url(&self) -> Url {
+        self.url("v1/mint/credit/quote")
+    }
+    pub fn lookup_credit_quote(&self, quote_id: &str) -> Url {
+        self.url(&format!("v1/mint/credit/quote/{}", quote_id))
+    }
+    pub fn list_keysets(&self) -> Url {
+        self.url("v1/keysets")
+    }
+}
+
+impl Endpoints<AdminService> {
+    pub fn keys_activate(&self) -> Url {
+        self.url("v1/admin/keys/activate")
+    }
+    pub fn admin_credit_quote(&self, quote_id: &str) -> Url {
+        self.url(&format!("v1/admin/credit/quote/{}", quote_id))
+    }
+}
+
+impl Endpoints<KeyService> {
+    pub fn mint_ebill(&self) -> Url {
+        self.url("v1/mint/ebill")
+    }
+}
