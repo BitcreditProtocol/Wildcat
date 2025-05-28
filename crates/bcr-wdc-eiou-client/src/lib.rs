009ab8b9146e8885ca6d91ab545e839021928d38
codingpeanut157 <codingpeanut15@gmail.com>
2025-05-28 12:51:47 +0200

    [bcr-wdc-eiou-client] add client for e-iou service

---
+++ crates/bcr-wdc-eiou-client/src/lib.rs
@@ -0,0 +1,40 @@
+// ----- standard library imports
+// ----- extra library imports
+use bcr_wdc_webapi::eiou as web_eiou;
+use thiserror::Error;
+// ----- local imports
+pub use reqwest::Url;
+
+// ----- end imports
+
+pub type Result<T> = std::result::Result<T, Error>;
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("internal error {0}")]
+    Reqwest(#[from] reqwest::Error),
+}
+
+#[derive(Debug, Clone)]
+pub struct EIOUClient {
+    cl: reqwest::Client,
+    base: reqwest::Url,
+}
+
+impl EIOUClient {
+    pub fn new(base: reqwest::Url) -> Self {
+        Self {
+            cl: reqwest::Client::new(),
+            base,
+        }
+    }
+
+    pub async fn balance(&self) -> Result<web_eiou::BalanceResponse> {
+        let url = self
+            .base
+            .join("v1/admin/eiou/balance")
+            .expect("eiou balance relative path");
+        let res = self.cl.get(url).send().await?;
+        let blnc = res.json::<web_eiou::BalanceResponse>().await?;
+        Ok(blnc)
+    }
+}
