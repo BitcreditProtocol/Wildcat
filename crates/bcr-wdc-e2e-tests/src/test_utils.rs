7d7c4837d7cc68dcd65b61fe873e44ef7c12a86f
codingpeanut157 <codingpeanut15@gmail.com>
2025-09-27 16:47:49 +0200

    [bcr-wdc-e2e-tests] make e2e tests use our clients
    still non-working tests

--- crates/bcr-wdc-e2e-tests/src/test_utils.rs
+++ crates/bcr-wdc-e2e-tests/src/test_utils.rs
@@ -1,38 +1,30 @@
-use bcr_wdc_webapi::test_utils::generate_random_bill_enquire_request;
 // ----- standard library imports
 // ----- extra library imports
-// ----- local modules
+use bcr_wdc_webapi::quotes as web_quotes;
+use bcr_wdc_webapi::test_utils::generate_random_bill_enquire_request;
 use bitcoin::secp256k1::Keypair;
 use cashu::nuts::nut02 as cdk02;
 use cashu::Amount;
+// ----- local modules
 // ----- end imports
 
-pub fn random_ebill_request() -> (
-    Keypair,
-    bcr_wdc_webapi::quotes::EnquireRequest,
-    bitcoin::secp256k1::schnorr::Signature,
-) {
-    let owner_key = bcr_wdc_utils::keys::test_utils::generate_random_keypair();
-    let (request, signing_kp) = generate_random_bill_enquire_request(owner_key, None);
-
-    let signature = bcr_wdc_utils::keys::schnorr_sign_borsh_msg_with_key(&request, &signing_kp)
-        .expect("schnorr_sign_borsh_msg_with_key");
-
-    (owner_key, request, signature)
+pub struct EbillRequestComponents {
+    pub bill: web_quotes::SharedBill,
+    pub owner_key: Keypair,
+    pub signing_key: Keypair,
 }
 
-pub fn get_amounts(mut targ: u64) -> Vec<u64> {
-    // TODO see if there is an existing cashu implementation
-    let mut coins = Vec::new();
-    let mut bit_position = 0;
-    while targ > 0 {
-        if (targ & 1) == 1 {
-            coins.push(1 << bit_position);
-        }
-        targ >>= 1;
-        bit_position += 1;
+pub fn random_ebill_request(
+    receiver: bitcoin::PublicKey,
+    amount: Option<bitcoin::Amount>,
+) -> EbillRequestComponents {
+    let owner_key = bcr_wdc_utils::keys::test_utils::generate_random_keypair();
+    let (request, signing_key) = generate_random_bill_enquire_request(receiver, None, amount);
+    EbillRequestComponents {
+        bill: request.content,
+        owner_key,
+        signing_key,
     }
-    coins
 }
 
 pub fn generate_blinds(
@@ -49,17 +41,4 @@ pub fn generate_blinds(
         blinds.push(blind);
     }
     blinds
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::test_utils::get_amounts;
-    #[test]
-    fn test_get_amounts() {
-        let amounts = get_amounts(1000);
-        let sum = amounts.iter().sum::<u64>();
-
-        assert_eq!(amounts, vec![8, 32, 64, 128, 256, 512]);
-        assert_eq!(sum, 1000);
-    }
 }
