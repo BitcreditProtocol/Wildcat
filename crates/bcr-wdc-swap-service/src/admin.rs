1b82752b397ea3d5785cf1e88e151755cde4b04d
codingpeanut157 <codingpeanut15@gmail.com>
2025-09-29 17:11:10 +0200

    [bcr-wdc-swap] integrate bcr-common::wire::swap

--- crates/bcr-wdc-swap-service/src/admin.rs
+++ crates/bcr-wdc-swap-service/src/admin.rs
@@ -1,7 +1,7 @@
 // ----- standard library imports
 // ----- extra library imports
 use axum::extract::{Json, State};
-use bcr_wdc_webapi::swap as web_swap;
+use bcr_common::wire::swap as wire_swap;
 // ----- local imports
 use crate::error::Result;
 use crate::service::Service;
@@ -10,12 +10,12 @@ use crate::service::{KeysService, ProofRepository};
 #[tracing::instrument(level = tracing::Level::DEBUG, skip(ctrl))]
 pub async fn recover_tokens<KeysSrvc, ProofRepo>(
     State(ctrl): State<Service<KeysSrvc, ProofRepo>>,
-    Json(request): Json<web_swap::RecoverRequest>,
-) -> Result<Json<web_swap::RecoverResponse>>
+    Json(request): Json<wire_swap::RecoverRequest>,
+) -> Result<Json<wire_swap::RecoverResponse>>
 where
     KeysSrvc: KeysService,
     ProofRepo: ProofRepository,
 {
     ctrl.recover(&request.proofs).await?;
-    Ok(Json(web_swap::RecoverResponse {}))
+    Ok(Json(wire_swap::RecoverResponse {}))
 }
