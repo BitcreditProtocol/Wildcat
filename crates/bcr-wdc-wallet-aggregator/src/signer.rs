d60073872aaa5663366261c690a567580e11b3f7
codingpeanut157 <codingpeanut15@gmail.com>
2025-11-05 15:32:25 +0100

    [bcr-wdc-wallet-aggregator] integrate ClowderSigner

--- crates/bcr-wdc-wallet-aggregator/src/signer.rs
+++ crates/bcr-wdc-wallet-aggregator/src/signer.rs
@@ -1,6 +1,7 @@
 // ----- standard library imports
 // ----- extra library imports
 use async_trait::async_trait;
+use clwdr_client::SignatoryNatsClient;
 // ----- local imports
 use crate::{
     commitment,
@@ -9,6 +10,7 @@ use crate::{
 
 // ----- end imports
 
+#[allow(dead_code)]
 #[derive(Default)]
 pub struct DummySigner {}
 
@@ -18,5 +20,21 @@ impl commitment::Signer for DummySigner {
         Err(Error::NotYet(
             "DummySigner does not implement signing".to_string(),
         ))
+    }
+}
+
+pub struct ClowderSigner(SignatoryNatsClient);
+impl ClowderSigner {
+    pub async fn new(url: reqwest::Url) -> Result<Self> {
+        let inner = SignatoryNatsClient::new(url, None).await?;
+        Ok(Self(inner))
+    }
+}
+
+#[async_trait]
+impl commitment::Signer for ClowderSigner {
+    async fn sign(&self, content: &[u8]) -> Result<bitcoin::secp256k1::schnorr::Signature> {
+        let signature = self.0.sign_bytes(content).await?;
+        Ok(signature)
     }
 }
