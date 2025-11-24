3022ccf33eb7caaceecbdbeb6027d11bdcd75cf5
codingpeanut157 <codingpeanut15@gmail.com>
2025-11-24 10:20:44 +0100

    [bcr-wdc-wallet-aggregator] v1/betas serves GET methods

--- crates/bcr-wdc-wallet-aggregator/src/lib.rs
+++ crates/bcr-wdc-wallet-aggregator/src/lib.rs
@@ -146,7 +146,7 @@ pub async fn routes(app: AppController) -> Result<Router> {
         .route("/v1/id", get(web::get_clowder_id))
         .route("/v1/path", post(web::post_clowder_path))
         .route("/v1/exchange", post(web::post_exchange))
-        .route("/v1/betas", post(web::get_clowder_betas))
+        .route("/v1/betas", get(web::get_clowder_betas))
         .with_state(app)
         .merge(swagger);
     Ok(router)
