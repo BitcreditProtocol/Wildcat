b560f5035f462395860281855f7ee7e64c8df894
codingpeanut157 <codingpeanut15@gmail.com>
2026-01-07 15:27:47 +0100

    [bcr-wdc-treasury-service] remove any credit balance

--- crates/bcr-wdc-treasury-service/src/admin.rs
+++ crates/bcr-wdc-treasury-service/src/admin.rs
@@ -3,65 +3,12 @@ use std::sync::Arc;
 // ----- extra library imports
 use axum::extract::{Json, State};
 use bcr_common::wire::signatures as wire_signatures;
-use bcr_wdc_webapi::{
-    exchange as web_exchange, signatures as web_signatures, wallet as web_wallet,
-};
+use bcr_wdc_webapi::{exchange as web_exchange, wallet as web_wallet};
 use cashu::{self as cdk};
 // ----- local imports
-use crate::{credit, debit, error::Result, foreign};
+use crate::{debit, error::Result, foreign};
 
 // ----- end imports
-
-// ----- crsat APIs
-#[tracing::instrument(level = tracing::Level::DEBUG, skip(ctrl))]
-pub async fn generate_blinds<Repo, KeySrvc>(
-    State(ctrl): State<credit::Service<Repo, KeySrvc>>,
-    Json(request): Json<web_signatures::GenerateBlindedMessagesRequest>,
-) -> Result<Json<web_signatures::GenerateBlindedMessagesResponse>>
-where
-    Repo: credit::Repository,
-{
-    tracing::debug!("Received request to generate blinds",);
-
-    let (rid, blinds) = ctrl.generate_blinds(request.kid, request.total).await?;
-    Ok(Json(web_signatures::GenerateBlindedMessagesResponse {
-        request_id: rid,
-        messages: blinds,
-    }))
-}
-
-#[tracing::instrument(level = tracing::Level::DEBUG, skip(ctrl))]
-pub async fn store_signatures<Repo, KeySrvc>(
-    State(ctrl): State<credit::Service<Repo, KeySrvc>>,
-    Json(request): Json<web_signatures::StoreBlindSignaturesRequest>,
-) -> Result<()>
-where
-    Repo: credit::Repository,
-{
-    tracing::debug!("Received request to store signatures",);
-
-    ctrl.store_signatures(request.rid, request.signatures)
-        .await?;
-    Ok(())
-}
-
-#[tracing::instrument(level = tracing::Level::DEBUG, skip(ctrl))]
-pub async fn crsat_balance<Repo, KeySrvc>(
-    State(ctrl): State<credit::Service<Repo, KeySrvc>>,
-) -> Result<Json<web_wallet::ECashBalance>>
-where
-    Repo: credit::Repository,
-    KeySrvc: credit::KeyService,
-{
-    tracing::debug!("Received request to crsat_balance");
-
-    let amount = ctrl.balance().await?;
-    let response = web_wallet::ECashBalance {
-        amount,
-        unit: cdk::CurrencyUnit::Custom("crsat".to_string()),
-    };
-    Ok(Json(response))
-}
 
 // ----- sat APIs
 #[tracing::instrument(level = tracing::Level::DEBUG, skip(ctrl))]
