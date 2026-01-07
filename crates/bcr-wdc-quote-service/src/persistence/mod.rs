05f60766367101ff28beb6271d6fbbe2f2760f74
codingpeanut157 <codingpeanut15@gmail.com>
2026-01-07 15:25:29 +0100

    [bcr-wdc-quote-service] refactor quote status
    add new `MintingEnabled` status

--- crates/bcr-wdc-quote-service/src/persistence/mod.rs
+++ crates/bcr-wdc-quote-service/src/persistence/mod.rs
@@ -1,8 +1,34 @@
 // ----- standard library imports
 // ----- extra library imports
+use async_trait::async_trait;
+use bcr_common::core::{BillId, NodeId};
+use uuid::Uuid;
 // ----- local modules
 pub mod inmemory;
 pub mod surreal;
 // ----- local imports
+use crate::{
+    error::Result,
+    quotes::{LightQuote, Quote, Status},
+    service::{ListFilters, SortOrder},
+    TStamp,
+};
 
 // ----- end imports
+
+#[cfg_attr(test, mockall::automock)]
+#[async_trait]
+pub trait Repository {
+    async fn load(&self, id: uuid::Uuid) -> Result<Option<Quote>>;
+    async fn update_status_if_pending(&self, id: uuid::Uuid, quote: Status) -> Result<()>;
+    async fn update_status_if_offered(&self, id: uuid::Uuid, quote: Status) -> Result<()>;
+    async fn update_status_if_accepted(&self, id: uuid::Uuid, quote: Status) -> Result<()>;
+    async fn list_pendings(&self, since: Option<TStamp>) -> Result<Vec<Uuid>>;
+    async fn list_light(
+        &self,
+        filters: ListFilters,
+        sort: Option<SortOrder>,
+    ) -> Result<Vec<LightQuote>>;
+    async fn search_by_bill(&self, bill: &BillId, endorser: &NodeId) -> Result<Vec<Quote>>;
+    async fn store(&self, quote: Quote) -> Result<()>;
+}
