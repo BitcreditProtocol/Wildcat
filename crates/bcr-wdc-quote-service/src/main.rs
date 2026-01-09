13a1a2a3e4839ccac3cb3b18a3331bb1134c11e3
stefanbitcr <stefanbitcr@gmail.com>
2026-01-09 10:36:44 +0100

    Add quote separate for config (#433)

--- crates/bcr-wdc-quote-service/src/main.rs
+++ crates/bcr-wdc-quote-service/src/main.rs
@@ -13,7 +13,7 @@ struct MainConfig {
 async fn main() {
     let settings = config::Config::builder()
         .add_source(config::File::with_name("config.toml"))
-        .add_source(config::Environment::with_prefix("QUOTE"))
+        .add_source(config::Environment::with_prefix("QUOTE").separator("__"))
         .build()
         .expect("Failed to build wildcat config");
 
