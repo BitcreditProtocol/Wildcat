910d908156ab05147a337ffeae2c26b05bf52876
codingpeanut157 <codingpeanut15@gmail.com>
2026-01-19 09:47:16 +0100

    [bcr-wdc-treasury-service] minor refactor + mint/melt changes
    Besides minor style refactoring, this commit adapts to new logic for
    minting/melting operations and communication with the ebpp service.

--- crates/bcr-wdc-treasury-service/src/debit/mod.rs
+++ crates/bcr-wdc-treasury-service/src/debit/mod.rs
@@ -1,14 +1,15 @@
 // ----- standard library imports
 // ----- extra library imports
 // ----- local modules
+mod clowder;
 mod service;
 mod wallet;
 mod wildcat;
-
 // ----- local imports
 
 // ----- end imports
 
-pub use service::{ClowderMintQuoteOnchain, MintQuote, OnchainMeltQuote, Repository, Service};
+pub use clowder::ClowderCl;
+pub use service::{ClowderMintQuoteOnchain, MintQuote, OnchainMeltQuote, Service};
 pub use wallet::{CDKWallet, CDKWalletConfig};
 pub use wildcat::{WildcatCl, WildcatClientConfig};
