81c6d8c97fe31775770ffe30edd86566cb6afedd
codingpeanut157 <codingpeanut15@gmail.com>
2026-01-19 09:47:16 +0100

    [bcr-wdc-treasury-service] minor refactor + mint/melt changes
    Besides minor style refactoring, this commit adapts to new logic for
    minting/melting operations and communication with the ebpp service.

--- crates/bcr-wdc-treasury-service/src/persistence/mod.rs
+++ crates/bcr-wdc-treasury-service/src/persistence/mod.rs
@@ -1,6 +1,30 @@
 // ----- standard library imports
 // ----- extra library imports
+use async_trait::async_trait;
+// ----- local imports
+use crate::{
+    debit::MintQuote,
+    debit::{ClowderMintQuoteOnchain, OnchainMeltQuote},
+    error::Result,
+};
 // ----- local modules
 pub mod inmemory;
 pub mod surreal;
-// ----- local imports
+
+// ----- end imports
+
+#[cfg_attr(test, mockall::automock)]
+#[async_trait]
+pub trait Repository: Send + Sync {
+    async fn store_quote(&self, quote: MintQuote) -> Result<()>;
+    async fn delete_quote(&self, qid: String) -> Result<()>;
+    async fn list_quotes(&self) -> Result<Vec<MintQuote>>;
+    async fn store_onchain_melt(&self, quote_id: uuid::Uuid, data: OnchainMeltQuote) -> Result<()>;
+    async fn load_onchain_melt(&self, quote_id: uuid::Uuid) -> Result<OnchainMeltQuote>;
+    async fn store_onchain_mint(
+        &self,
+        quote_id: uuid::Uuid,
+        data: ClowderMintQuoteOnchain,
+    ) -> Result<()>;
+    async fn load_onchain_mint(&self, quote_id: uuid::Uuid) -> Result<ClowderMintQuoteOnchain>;
+}
