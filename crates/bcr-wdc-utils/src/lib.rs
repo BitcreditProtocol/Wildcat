b5c7698f57879a597d2a1d3b8e5c6e5c4101d2b7
codingpeanut157 <codingpeanut15@gmail.com>
2026-01-23 11:12:53 +0100

    [bcr-wdc-utils] moved version gathering functions from wallet-aggregator

--- crates/bcr-wdc-utils/src/lib.rs
+++ crates/bcr-wdc-utils/src/lib.rs
@@ -5,9 +5,15 @@ pub mod borsh;
 #[cfg(feature = "auth")]
 pub mod client;
 pub mod convert;
+pub mod info;
 pub mod keys;
 pub mod signatures;
+pub mod built_info {
+    // The file has been placed there by the build script.
+    include!(concat!(env!("OUT_DIR"), "/built.rs"));
+}
 // ----- local imports
 
 // ----- end imports
+
 pub use crate::keys::KeysetEntry;
