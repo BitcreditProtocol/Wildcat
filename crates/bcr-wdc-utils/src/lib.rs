9afa2c7b1d4344b892d7c326fc1bee5326d37add
codingpeanut157 <codingpeanut15@gmail.com>
2026-02-20 11:18:25 +0100

    [bcr-wdc-utils] add surreal DBConnConfig struct

--- crates/bcr-wdc-utils/src/lib.rs
+++ crates/bcr-wdc-utils/src/lib.rs
@@ -7,6 +7,7 @@ pub mod convert;
 pub mod info;
 pub mod keys;
 pub mod signatures;
+pub mod surreal;
 pub mod built_info {
     // The file has been placed there by the build script.
     include!(concat!(env!("OUT_DIR"), "/built.rs"));
