dfac64e51b4d4cc4f199022618468937dfd1b1e9
codingpeanut157 <codingpeanut15@gmail.com>
2026-02-24 12:03:32 +0100

    [bcr_wdc_core_service] replace clients with core::Client

--- crates/bcr-wdc-core-service/tests/keyset_info.rs
+++ crates/bcr-wdc-core-service/tests/keyset_info.rs
@@ -1,7 +1,7 @@
 // ----- standard library imports
 // ----- extra library imports
 use bcr_common::{
-    client::keys::{Client as KeysClient, Error as KeysError},
+    client::core::{Client as CoreClient, Error as CoreError},
     core_tests,
 };
 // ----- local imports
@@ -10,13 +10,13 @@ use bcr_common::{
 async fn keyset_info_not_found() {
     let (server, _) = bcr_wdc_core_service::test_utils::build_test_server(None).await;
     let server_url = server.server_address().expect("address");
-    let client = KeysClient::new(server_url);
+    let client = CoreClient::new(server_url);
 
     let kid = core_tests::generate_random_ecash_keyset().0.id;
     let response = client.keyset_info(kid).await;
     assert!(response.is_err());
     assert!(matches!(
         response.unwrap_err(),
-        KeysError::KeysetIdNotFound(_)
+        CoreError::KeysetIdNotFound(_)
     ));
 }
