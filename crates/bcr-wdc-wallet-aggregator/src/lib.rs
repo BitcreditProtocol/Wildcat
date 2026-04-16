4f6ab462a5f5d5fd9c4fed0ad6164d62a6bc0f6e
codingpeanut157 <codingpeanut15@gmail.com>
2026-04-16 11:03:18 +0200

    [bcr-wdc-wallet-aggregator] remove empty wrappers
    some of the API that the wallet aggregator used to provide no longer
    make sense.
    There is no need to aggregate such functions any more, we can simply
    redirect such APIs directly to the internal services

--- crates/bcr-wdc-wallet-aggregator/src/lib.rs
+++ crates/bcr-wdc-wallet-aggregator/src/lib.rs
@@ -6,16 +6,8 @@ use axum::{
     routing::{get, post},
     Router,
 };
-use bcr_common::{
-    cashu,
-    client::clowder::Client as ClowderClient,
-    wire::{
-        clowder::{self as wire_clowder},
-        exchange as wire_exchange,
-    },
-};
+use bcr_common::client::{clowder::Client as ClowderClient, core::Client as CoreClient};
 use bcr_wdc_utils::surreal;
-use utoipa::OpenApi;
 // ----- local modules
 mod commitment;
 mod error;
@@ -92,28 +84,13 @@ impl AppController {
 }
 
 pub async fn routes(app: AppController) -> Result<Router> {
-    let swagger = utoipa_swagger_ui::SwaggerUi::new("/swagger-ui")
-        .url("/api-docs/openapi.json", ApiDoc::openapi());
-
     let router = Router::new()
         .route("/health", get(web::health))
         // Cashu Endpoints
         .route("/v1/info", get(web::get_mint_info))
         .route("/v1/wildcat", get(web::get_wildcat_info))
-        .route("/v1/keys", get(web::get_mint_keys))
-        .route("/v1/keysets", get(web::get_mint_keysets))
-        .route("/v1/keysets/{kid}", get(web::get_keyset_info))
-        .route("/v1/keys/{kid}", get(web::get_mint_keyset))
-        .route("/v1/swap", post(web::post_swap))
-        .route("/v1/checkstate", post(web::post_check_state))
-        .route("/v1/restore", post(web::post_restore))
-        .route("/v1/swap/commitment", post(web::post_commit))
+        .route(CoreClient::SWAP_EP_V1, post(web::post_swap))
         // Clowder Endpoints
-        .route(ClowderClient::LOCAL_INFO_EP_V1, get(web::get_clowder_info))
-        .route(
-            ClowderClient::LOCAL_PATH_EP_V1,
-            post(web::post_clowder_path),
-        )
         .route(
             ClowderClient::ONLINE_EXCHANGE_EP_V1,
             post(web::post_online_exchange),
@@ -122,77 +99,7 @@ pub async fn routes(app: AppController) -> Result<Router> {
             ClowderClient::OFFLINE_EXCHANGE_EP_V1,
             post(web::post_offline_exchange),
         )
-        .route(
-            ClowderClient::LOCAL_BETAS_EP_V1,
-            get(web::get_clowder_betas),
-        )
-        .route(
-            ClowderClient::FOREIGN_OFFLINE_EP_V1,
-            get(web::get_foreign_offline),
-        )
-        .route(
-            ClowderClient::FOREIGN_STATUS_EP_V1,
-            get(web::get_foreign_status),
-        )
-        .route(
-            ClowderClient::FOREIGN_SUBSTITUTE_EP_V1,
-            get(web::get_foreign_substitute),
-        )
-        .route(
-            ClowderClient::FOREIGN_KEYSETS_EP_V1,
-            get(web::get_foreign_keysets),
-        )
         .route(ClowderClient::LOCAL_COVERAGE_EP_V1, get(web::get_coverage))
-        .route(
-            ClowderClient::LOCAL_DERIVE_EBILL_PAYMENT_ADDRESS_EP_V1,
-            get(web::post_derive_ebill_payment_address),
-        )
-        .with_state(app)
-        .merge(swagger);
+        .with_state(app);
     Ok(router)
 }
-
-#[derive(utoipa::OpenApi)]
-#[openapi(
-    components(schemas(
-        // clowder service
-        wire_clowder::OfflineResponse,
-        wire_clowder::AlphaStateResponse,
-        wire_clowder::ConnectedMintResponse,
-        wire_clowder::ConnectedMintsResponse,
-        wire_clowder::PathRequest,
-        wire_clowder::ClowderNodeInfo,
-        wire_clowder::Coverage,
-        bcr_common::wire::info::WildcatInfo,
-        bcr_common::wire::info::VersionInfo,
-        // exchange service
-        wire_exchange::OnlineExchangeRequest,
-        wire_exchange::OnlineExchangeResponse,
-        wire_exchange::OfflineExchangeRequest,
-        wire_exchange::OfflineExchangeResponse,
-        // cashu types
-        cashu::KeysResponse,
-    )),
-    paths(
-        crate::web::health,
-        crate::web::get_wildcat_info,
-        crate::web::get_mint_keys,
-        crate::web::get_mint_keysets,
-        crate::web::get_mint_keyset,
-        crate::web::post_swap,
-        crate::web::post_check_state,
-        crate::web::post_restore,
-        crate::web::get_clowder_info,
-        crate::web::post_clowder_path,
-        crate::web::get_clowder_betas,
-        crate::web::get_foreign_offline,
-        crate::web::get_foreign_status,
-        crate::web::get_foreign_substitute,
-        crate::web::get_foreign_keysets,
-        crate::web::post_online_exchange,
-        crate::web::post_offline_exchange,
-        crate::web::get_coverage,
-        crate::web::post_derive_ebill_payment_address,
-    )
-)]
-struct ApiDoc;
