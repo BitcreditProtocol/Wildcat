a8a051311a5bc586c6edf396d05cd6e101c290fe
codingpeanut157 <codingpeanut15@gmail.com>
2026-09-03 16:16:29 +0200

    [bcr-wdc-e2e-tests][bcr_wdc_webapi] remove'em
    e2e tests are unused and unusable as wildcat now heavily depends on
    clowder-node running
    
    webapi only contained a test function used by e2e tests

--- crates/bcr-wdc-webapi/src/lib.rs
+++
@@ -1,8 +0,0 @@
-// ----- standard library imports
-// ----- extra library imports
-// ----- local modules
-pub mod signatures;
-#[cfg(feature = "test-utils")]
-pub mod test_utils;
-
-// ----- end imports
